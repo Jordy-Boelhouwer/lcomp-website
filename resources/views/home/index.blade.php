5554050486d2a9112a859b483685712d2d8affc6
Jor\Jordy <jordyboelhouwer@hotmail.com>
2018-07-14 11:38:05 +0200

    lang file changes + content changes on home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,21 +9,15 @@
 				@include('partials/leftsidebar/hulp')
 				@include('partials/leftsidebar/partners')
 			</div>
-            <div class="col-lg-7 col-md-12">
+            <div class="col-lg-7 col-sm-12">
                 <h1>L’Comp Automatiseringen</h1>
 				<p>
 					<b>
-						Automatisering en Informatievoorziening ondersteunt het functioneren binnnen een organisatie en speelt hiermee een belangrijke rol in effectiviteit.
-						Het inrichten, beheren en functioneren zijn ‘de tools’ van de ICT infrastructuur.
-						Ook het up-to-date houden van applicaties is van belang omdat de technische ontwikkelingen elkaar snel op volgen.
-						Daarnaast dienen de kosten aanvaardbaar te blijven.
-						Voor middelgrote en kleinere organisaties is het vaak onbetaalbaar om een fulltime netwerk/systeembeheerder in dienst te nemen.
+						@lang('home.text-intro')
 					</b>
 				</p>
 				<p>
-					L’Comp biedt u de oplossing om zonder een vaste kracht een passende automatisering en informatievoorziening in huis te hebben.
-					Wij zijn een jonge onderneming en wij hebben de benodigde kennis en ervaring op het gebied van systeem- en netwerkbeheer.
-					Dit te samen met onze service en ondersteuning bieden wij u een, in de markt unieke, totale service; “alles in een hand”
+					@lang('home.text')
 				</p>
 				<img class="w-100" alt="" src="{{ asset('/images/LCOMPTeamNieuw1.jpg') }}">
 			</div>
--- resources/views/partials/leftsidebar/hulp.blade.php
+++ resources/views/partials/leftsidebar/hulp.blade.php
@@ -1,16 +1,20 @@
-<h5>Hulp nodig</h5>
+<h5>Hulp nodig?</h5>
 <hr>
 <div class="sidepadding">
 	<div class="card">
 		<div class="sidecard-body">
-			<div class="text-center"><b>Tel. 020 482 4440</b></div>
+			<div class="text-center d-flex align-items-center">
+				<i class="mdi mdi-phone mdi-18px mr-2 text-primary"></i>
+				<b><a href="tel:0204824440">020 482 4440</a></b>
+			</div>
 		</div>
 	</div>
 </div>
 <div class="sidepadding">
 	<div class="card">
-		<div class="sidecard-body">
-			<a href="mailto:info@lcomp.nl" class="btn btn-primary w-100">Email</a>
+		<div class="sidecard-body d-flex align-items-center">
+			<i class="mdi mdi-email mdi-18px mr-2 text-primary"></i>
+			<b><a href="mailto:info@lcomp.nl">Stuur een email</a></b>
 		</div>
 	</div>
 </div>
