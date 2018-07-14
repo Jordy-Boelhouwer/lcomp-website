d459af0fcab5aee6044acbb37ce7fd895eb6949f
Jor\Jordy <jordyboelhouwer@hotmail.com>
2018-07-14 13:40:07 +0200

    small changes

--- resources/views/partials/leftsidebar/hulp.blade.php
+++ resources/views/partials/leftsidebar/hulp.blade.php
@@ -1,20 +1,15 @@
 <h5>Hulp nodig?</h5>
 <hr>
 <div class="sidepadding">
-	<div class="card">
-		<div class="sidecard-body">
-			<div class="text-center d-flex align-items-center">
-				<i class="mdi mdi-phone mdi-18px mr-2 text-primary"></i>
-				<b><a href="tel:0204824440">020 482 4440</a></b>
-			</div>
-		</div>
+	<div class="d-flex align-items-center">
+		<i class="mdi mdi-phone mdi-18px mr-2 text-primary"></i>
+		<b><a href="tel:0204824440">020 482 4440</a></b>
 	</div>
 </div>
+
 <div class="sidepadding">
-	<div class="card">
-		<div class="sidecard-body d-flex align-items-center">
-			<i class="mdi mdi-email mdi-18px mr-2 text-primary"></i>
-			<b><a href="mailto:info@lcomp.nl">Stuur een email</a></b>
-		</div>
+	<div class="d-flex align-items-center">
+		<i class="mdi mdi-email mdi-18px mr-2 text-primary"></i>
+		<b><a href="mailto:info@lcomp.nl">Stuur een email</a></b>
 	</div>
 </div>
