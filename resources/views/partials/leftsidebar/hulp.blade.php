e86b91ea766ad97c253c8be002149edd60c07ee9
Jor\Jordy <jordyboelhouwer@hotmail.com>
2018-07-21 12:43:21 +0200

    fix after merge

--- resources/views/partials/leftsidebar/hulp.blade.php
+++ resources/views/partials/leftsidebar/hulp.blade.php
@@ -1,15 +1,16 @@
 <h6>@lang('home.need-help')</h6>
 <div class="sidepadding">
-	<div class="d-flex align-items-center">
-		<div class="d-flex align-items-center">
-			<i class="mdi mdi-phone mdi-18px mr-2 text-primary"></i>
-			<b><a href="tel:0204824440">020 482 4440</a></b>
-		</div>
+	<div class="card">
+		<div class="sidecard-body">
+			<div class="d-flex align-items-center">
+				<i class="mdi mdi-phone mdi-18px mr-2 text-primary"></i>
+				<b><a href="tel:0204824440">020 482 4440</a></b>
+			</div>
 
-		<div class="d-flex align-items-center mt-2">
-			<i class="mdi mdi-email mdi-18px mr-2 text-primary"></i>
-			<b><a href="mailto:info@lcomp.nl">@lang('home.send-email')</a></b>
-		</div>
+			<div class="d-flex align-items-center mt-2">
+				<i class="mdi mdi-email mdi-18px mr-2 text-primary"></i>
+				<b><a href="mailto:info@lcomp.nl">@lang('home.send-email')</a></b>
+			</div>
 		</div>
 	</div>
 </div>
