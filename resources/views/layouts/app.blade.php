2d99cce793d1117509ffe7405b56592429408d97
Jor\Jordy <jordyboelhouwer@hotmail.com>
2018-09-01 10:34:53 +0200

    added cookie message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,30 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
 
     <title>@yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+    <script>
+        window.addEventListener("load", function(){
+            window.cookieconsent.initialise({
+                "palette": {
+                    "popup": {
+                        "background": "#237afc"
+                    },
+                    "button": {
+                        "background": "#fff",
+                        "text": "#237afc"
+                    }
+                },
+                "theme": "classic",
+                "position": "bottom-right",
+                "content": {
+                    "message": "Lcomp.nl gebruikt cookies om het gebruik van de website gemakkelijker te maken. Bezoek je onze website, dan ga je akkoord met deze ",
+                    "dismiss": "Verberg deze melding",
+                    "link": "Cookies",
+                    "href": "www.lcomp.nl/privacyverklaring"
+                }
+            })});
+    </script>
 </head>
 <body>
 @include('partials/navbar')
